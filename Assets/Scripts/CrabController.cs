48f36ddbdc782a1bb1f45e0e2a50463b26338b3b
Christo Mabbs <christo@digitalnoir.com.au>
2019-01-25 19:52:11 +1030

    setting up basic crab movement and various others

--- Assets/Scripts/CrabController.cs
+++ Assets/Scripts/CrabController.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrabController : MonoBehaviour {
+public class CrabController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    ////////////////
+    // References //
+    ////////////////
+
+    Rigidbody2D Rigidbody2DRef;
+
+    ///////////
+    // Setup //
+    ///////////
+
+    void Awake()
+    {
+
+        // Grab useful references
+        Rigidbody2DRef = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    ///////////////////
+    // Crab movement //
+    ///////////////////
+
+    public void turnCrab(bool turnDirection)
+    {
+
+
+
+    }
+
+    public void moveCrab(bool moveDirection)
+    {
+
+
+
+    }
+
 }
