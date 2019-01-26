53f0bafc97b4df869e0b990e70a8837384925bea
dmerenda <d.merenda@outlook.com>
2019-01-26 18:39:12 +1030

    *Shell Stats Controller

---
+++ Assets/ShellStatsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellStatsController : MonoBehaviour
+{
+    public float movementSpeed = 1;
+    public float attackDamage = 5;
+    public float defencePoints = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
