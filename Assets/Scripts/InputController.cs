fbe391f02f52daa54432bb7ae28d562180da31c2
dmerenda <d.merenda@outlook.com>
2019-01-26 15:57:11 +1030

    *Setup 4 player controllers

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -44,7 +44,10 @@ public class InputController : MonoBehaviour
         // If the player can control the crabs
         if (PlayerCanControlCrabs)
         {
-
+            //Movement for player 1
+            // W
+            //A D
+            // S
             // Detect player turn
             if (Input.GetKey(KeyCode.A))
             {
@@ -54,7 +57,6 @@ public class InputController : MonoBehaviour
             {
                 CrabControllerRef[0].turnCrab(true);
             }
-
             // Detect player move
             if (Input.GetKey(KeyCode.W))
             {
@@ -66,6 +68,83 @@ public class InputController : MonoBehaviour
                 CrabControllerRef[0].moveCrab(false);
             }
 
+
+
+            //Movement for player 2
+            // I
+            //J L
+            // K
+            // Detect player turn
+            if (Input.GetKey(KeyCode.J))
+            {
+                CrabControllerRef[1].turnCrab(false);
+            }
+            else if (Input.GetKey(KeyCode.L))
+            {
+                CrabControllerRef[1].turnCrab(true);
+            }
+            // Detect player move
+            if (Input.GetKey(KeyCode.I))
+            {
+                CrabControllerRef[1].moveCrab(true);
+
+            }
+            else if (Input.GetKey(KeyCode.K))
+            {
+                CrabControllerRef[1].moveCrab(false);
+            }
+
+
+            //Movement for player 3
+            //    Up
+            //Left  Right
+            //   Down
+            // Detect player turn
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                CrabControllerRef[2].turnCrab(false);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                CrabControllerRef[2].turnCrab(true);
+            }
+
+            // Detect player move
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                CrabControllerRef[2].moveCrab(true);
+
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                CrabControllerRef[2].moveCrab(false);
+            }
+
+
+            //Movement for player 4
+            // 8
+            //4 6
+            // 5
+            // Detect player turn
+            if (Input.GetKey(KeyCode.Keypad4))
+            {
+                CrabControllerRef[3].turnCrab(false);
+            }
+            else if (Input.GetKey(KeyCode.Keypad6))
+            {
+                CrabControllerRef[3].turnCrab(true);
+            }
+
+            // Detect player move
+            if (Input.GetKey(KeyCode.Keypad8))
+            {
+                CrabControllerRef[3].moveCrab(true);
+
+            }
+            else if (Input.GetKey(KeyCode.Keypad5))
+            {
+                CrabControllerRef[3].moveCrab(false);
+            }
         }
 
     }
